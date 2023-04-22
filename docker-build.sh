06b6ee4f18f1064729175b4f83beaf0459424276
Scott K Gregory <scott.gregory224@gmail.com>
2023-04-22 12:11:52 +0100

    Fix docker build script

--- docker-build.sh
+++ docker-build.sh
@@ -2,15 +2,13 @@
 
 set -ex
 
-PARENT_DIR=$(basename "${PWD%/*}")
 CURRENT_DIR="${PWD##*/}"
-IMAGE_NAME="$PARENT_DIR/$CURRENT_DIR"
-TAG=$(git describe --long --candidates=1)
+TAG=$(git rev-parse --short HEAD)
 
 REGISTRY="hub.docker.com"
 
-docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} .
-docker tag ${REGISTRY}/${IMAGE_NAME}:${TAG} ${REGISTRY}/${IMAGE_NAME}:latest
-docker push ${REGISTRY}/${IMAGE_NAME}
-docker tag ${REGISTRY}/${IMAGE_NAME}:latest ${REGISTRY}/${IMAGE_NAME}:${TAG}
-docker push ${REGISTRY}/${IMAGE_NAME}
+docker build --platform linux/amd64 -t ${REGISTRY}/${CURRENT_DIR}:${TAG} .
+docker tag ${REGISTRY}/${CURRENT_DIR}:${TAG} ${REGISTRY}/${CURRENT_DIR}:latest
+docker push ${REGISTRY}/${CURRENT_DIR}
+docker tag ${REGISTRY}/${CURRENT_DIR}:latest ${REGISTRY}/${CURRENT_DIR}:${TAG}
+docker push ${REGISTRY}/${CURRENT_DIR}
